661dc1a50d6cc76781f317c2e98efd2b1badcfd9
Marty <martynolan17@gmail.com>
2022-07-25 11:06:11 -0400

    added function to create a README

---
+++ README.md
@@ -0,0 +1,23 @@
+
+  
+  # Top Stocks
+
+  ## Description 
+  Top Stocks is a basic stock scanner that can pull data and new on any stock.
+
+  ## Table Of Contents
+
+  ## Installation
+  Not sure.
+
+  ## Usage
+  Not right now.
+
+  ## Credits
+  Will and Khris
+
+  ## License
+  Apache 2.0
+
+  ## Tests
+  Not sure.
--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-inquirer
-const questions = () => {
-    return inquirer.prompt([
+const questions = [
         {
             type:"input", 
             name: "name", 
@@ -64,21 +62,28 @@ const questions = () => {
             name: "email", 
             message: "Please provide your email. (Required)"
         }
-
-    ])
-    .then(questions => console.log(questions));
-}
-
-questions();
+    ]
 
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+
+        console.log("README complete!")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((userAnswers) => {
+        const data = generateMarkdown(userAnswers);
+        console.log(userAnswers);
+        writeToFile("README.md", data);
+    })
+}
 
 // Function call to initialize app
 init();
